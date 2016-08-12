3a97497ae3ac320aaaa19f25b59cd5f71bfc4762
ltebean <yucong1118@gmail.com>
2016-08-12 17:49:21 +0800

    init

--- LTCircleView/ViewController.swift
+++ LTCircleView/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circleView: LTCircleView!
+    var items = ["1"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        circleView.dataSource = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        circleView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func buttonAddPressed(sender: AnyObject) {
+        let index = items.count
+        items.insert("t", atIndex: index)
+        circleView.insertItemAtIndex(index)
     }
 
+    @IBAction func buttonRemovePressed(sender: AnyObject) {
+        let index = 2
+        items.removeAtIndex(index)
+        circleView.removeItemAtIndex(index)
 
+    }
+}
+
+extension ViewController: LTCircleViewDataSource {
+    
+    func numberOfItemsInCircleView(circleView: LTCircleView) -> Int {
+        return items.count
+    }
+    
+    func viewAtIndex(index: Int, inCircleView circleView: LTCircleView) -> UIView {
+        let label = UILabel(frame: CGRectZero)
+        label.frame.size = CGSize(width: 30, height: 30)
+        label.backgroundColor = UIColor.redColor()
+        label.textAlignment = .Center
+        label.text = items[index]
+        return label
+    }
 }
 
