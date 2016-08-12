38133d91e6d74e02cbc9a31dafc9b087a120355d
ltebean <yucong1118@gmail.com>
2016-08-12 10:31:22 +0800

    Initial Commit

---
+++ LTCircleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LTCircleView
+//
+//  Created by leo on 16/8/12.
+//  Copyright © 2016年 io.ltebean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
